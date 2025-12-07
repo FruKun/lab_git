7abc486d0eb3607ce7e94f8a77e46fbadc760cae
soullsi <vadim.ulik.1998@yandex.ru>
2025-12-07 21:05:39 +0500

    написаны функции для чтения строк и их вывода

--- new_func.cpp
+++ new_func.cpp
@@ -1,16 +1,28 @@
+#include "new_func.h"
 #include <iostream>
-#include <vector>
 #include <fstream>
-void read(std::string fileInput, std::vector<std::string> &list)
-{
-    return;
+
+void read(std::string fileInput, std::vector<std::string> &list) {
+    std::ifstream inputFile(fileInput);
+
+    if (!inputFile.is_open()) {
+        std::cout << "file is not open" << fileInput << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        list.push_back(line);
+    }
+
+    inputFile.close();
 }
 
-void print(std::vector<std::string> &list)
-{
-    return;
+void print(std::vector<std::string> &list) {
+    for (size_t i = 0; i < list.size(); ++i) {
+        std::cout << i + 1 << ". " << list[i] << std::endl;
+    }
 }
-void write(std::string fileOutput, std::vector<std::string> &list)
-{
-    return;
+
+void write(std::string fileOutput, std::vector<std::string> &list) {
 }
